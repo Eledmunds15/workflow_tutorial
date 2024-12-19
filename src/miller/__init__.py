fd98129bc5890682f839a20c186c25881242c832
Ethan Edmunds <ethanedmunds15@gmail.com>
2024-12-19 16:10:38 +0000

    filename input stuff...

--- src/miller/__init__.py
+++ src/miller/__init__.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 
 
-def flux_surface(R0, delta, kappa, A):
+def flux_surface(R0, delta, kappa, alpha):
     """flux_surface
 
     Args
@@ -16,14 +17,14 @@ def flux_surface(R0, delta, kappa, A):
     """
 
     theta = np.linspace(0, 2 * np.pi)
-    r = R0 / A
+    r = R0 / alpha
     R_s = R0 + r * np.cos(theta + (np.arcsin(delta) * np.sin(theta)))
     Z_s = kappa * r * np.sin(theta)
 
     return [R_s, Z_s]
 
 
-def plot_surface(ans, savefig=True):
+def plot_surface(filename, ans, savefig=True):
     """plot_surface
 
     Args
@@ -43,17 +44,31 @@ def plot_surface(ans, savefig=True):
     plt.xlabel("R [m]")
     plt.ylabel("Z [m]")
 
-    plt.savefig("miller.png")
+    plt.savefig(filename)
 
 
 def main():
-    A = 2.2
-    kappa = 1.5
-    delta = 0.3
-    R0 = 2.5
+    parser = argparse.ArgumentParser(
+        prog="Miller",
+        description="Takes some input and does some calcs!",
+        epilog="Help arrived!!",
+    )
 
-    param = flux_surface(R0, delta, kappa, A)
-    plot_surface(param)
+    parser.add_argument("--filename", help="Help text")
+    parser.add_argument(
+        "-R", "--R0", help="Help text", default=2.5, type=float
+    )  # positional argument
+    parser.add_argument(
+        "-D", "--delta", help="Help text", default=0.3, type=float
+    )  # option that takes a value
+    parser.add_argument("-K", "--kappa", help="Help text", default=1.5, type=float)
+    parser.add_argument("-A", "--A", help="Help text", default=2.2, type=float)
+
+    args = parser.parse_args()
+    print(args.R0, args.delta, args.kappa, args.A)
+
+    param = flux_surface(args.R0, args.delta, args.kappa, args.A)
+    plot_surface(args.filename, param)
 
 
 if __name__ == "__main__":
